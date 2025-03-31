cf2e1c27ce3f7b5ddb05c045807fbad7648b68f1
Armaan Zeyad <109156434+User-1289@users.noreply.github.com>
2025-03-31 08:20:27 +0530

    Update product-demo.tsx

--- src/components/landing/product-demo.tsx
+++ src/components/landing/product-demo.tsx
@@ -4,7 +4,7 @@ export default function ProductDemo() {
   return (
     <div id="product-demo" className="w-screen h-screen flex justify-center items-center">
       <iframe
-        src="https://drive.google.com/file/d/1LcAUAwyRmUEHTKzPejEPN3G4AH1kNSO1/preview"
+        src="https://drive.google.com/file/d/1yjySOORK4ETiDjpDZHnWXYTW1SLZEJf9/preview"
         className="w-full h-full"
         allow="autoplay"
       ></iframe>
